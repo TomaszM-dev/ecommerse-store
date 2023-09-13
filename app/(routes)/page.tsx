3550bf2469e98580a6f10c16597e8cb42da5f2c8
TomaszM-dev <tomekmalocha@gmail.com>
2023-09-14 01:23:50 +0200

    creating home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+export default async function Home() {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("cd81822f-8da1-4369-a279-5a239121a561");
   return (
-    <div>
-      <h1>hello store</h1>
-    </div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,20 @@
+import getCategories from "@/actions/get-categories";
 import Link from "next/link";
 import React from "react";
 import MainNav from "./main-nav";
+import NavbarActions from "./navbar-actions";
 import Container from "./ui/container";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const categories = await getCategories();
   return (
     <Container>
       <div className="relative px-4 sm:px-6 lg:px-8 h-16 items-center flex">
         <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
           <p className="font-bold text-xl">STORE</p>
         </Link>
-        <MainNav data={[]} />
+        <MainNav data={categories} />
+        <NavbarActions />
       </div>
     </Container>
   );
